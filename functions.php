3a4cb910bfdd4030d881b1edd1b6e1472ebe87e2
Sean <sean.ooi87@gmail.com>
2013-08-23 02:14:33 -0500

    Team page styling

--- functions.php
+++ functions.php
@@ -98,6 +98,12 @@
 			echo '</li>';
 		}
 	}
+	
+	function filter_ptags_on_images($content){
+	   return preg_replace('/<p>\\s*?(<a .*?><img.*?><\\/a>|<img.*?>)?\\s*<\\/p>/s', '\1', $content);
+	}
+	
+	add_filter('the_content', 'filter_ptags_on_images');
 
 	/* ========================================================================================================================
 	
---
+++ team-page.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Team Page
+*/
+?>
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+<article id="defaultPage" class="emp_team">
+    <?php if (have_posts()) : while (have_posts()) : the_post();?>
+    <div class="post">
+    	<div id="bannerImg"></div>
+        <div class="entrytext">
+            <?php the_content(); ?>
+        </div>
+    </div>
+    <?php endwhile; endif; ?>
+</article>
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-footer' ) ); ?>
